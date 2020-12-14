42cc1269742dfd42c15a1edddb8048a8a516ed73
aske477 <aske07@gmail.com>
2020-12-15 00:43:42 +0800

    切換頁面&傳送資料功能

--- app/src/main/java/com/example/ordersystem/MainActivity.kt
+++ app/src/main/java/com/example/ordersystem/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.ordersystem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_choice.setOnClickListener { startActivity(Intent(this,MainActivity2::class.java)) }
+
+        intent?.extras?.let {
+            val data1 = it.getString("name")
+            val data2 = it.getString("suger")
+            val data3 = it.getString("ice")
+            order.text = "飲料:"+data1+"\n\n甜度:"+data2+"\n\n冰塊:"+data3
+        }
     }
 }
--- app/src/main/java/com/example/ordersystem/MainActivity2.kt
+++ app/src/main/java/com/example/ordersystem/MainActivity2.kt
@@ -1,11 +1,32 @@
 package com.example.ordersystem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        button_check.setOnClickListener {
+            if (drink_name.length()<1)
+                Toast.makeText(this,"請輸入飲料名稱", Toast.LENGTH_SHORT).show()
+            else{
+                val bundle = Bundle() //宣告bundle
+                bundle.putString("name", drink_name.text.toString())    //放入飲料名稱
+                val sugerchosen = suger.findViewById<RadioButton>(suger.checkedRadioButtonId).text
+                bundle.putString("suger",sugerchosen.toString()) //放入甜度
+                val icechosen = ice.findViewById<RadioButton>(ice.checkedRadioButtonId).text
+                bundle.putString("ice",icechosen.toString())    //放入冰度
+
+                val intent = Intent(this,MainActivity::class.java)  //宣告意圖
+                intent.putExtras(bundle)
+                startActivity(intent)
+            }
+        }
     }
 }
